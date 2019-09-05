091a457880e1d3291b9114894d93ad0bc2d0a1eb
xiongwei <2894001817@qq.com>
2019-09-05 17:59:36 +0800

    添加了addform.vue并调通了post的请求

--- src/main.js
+++ src/main.js
@@ -1,12 +1,18 @@
 import Vue from 'vue'
+import Vuex from 'vuex'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import Antd from 'ant-design-vue'
+import 'ant-design-vue/dist/antd.css'
 
 Vue.config.productionTip = false
+const vueStore = new Vuex.Store(store)
+Vue.use(Antd)
+Vue.use(Vuex)
 
 new Vue({
   router,
-  store,
+  store:vueStore,
   render: h => h(App)
 }).$mount('#app')
--- src/store.js
+++ src/store.js
@@ -1,9 +1,9 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-
+import axios from "axios"
 Vue.use(Vuex)
 
-export default new Vuex.Store({
+export default {
   state: {
 
   },
@@ -11,6 +11,36 @@ export default new Vuex.Store({
 
   },
   actions: {
-
+    fetchTodos(context) {
+      const url = "http://5b4dcb2aec112500143a2311.mockapi.io/api/todos";
+      context.commit('startLoadding');
+      axios.get(url).then(function (reponse) {
+        context.commit('completeLoadding');
+        context.commit('initTodos', reponse.data);
+      }).catch(function (error) {
+        alert(error.reponse)
+      })
+    },
+    createTodos(context, content) {
+      console.log(content)
+      const url = "http://192.168.8.107:8099/packages";
+      axios.post(url, content).then(function (reponse) {
+        context.dispatch('fetchTodos');
+      }).catch(function (error) {
+        alert(error)
+      })
+    },
+    updateTodos(context, index) {
+      const url = "http://5b4dcb2aec112500143a2311.mockapi.io/api/todos/" + index;
+      axios.put(url, {
+        "id": "",
+        "content": "默认修改值",
+        "status": "active"
+      }).then(function (reponse) {
+        context.dispatch('fetchTodos');
+      }).catch(function (error) {
+        alert(error)
+      })
+    }
   }
-})
+}
